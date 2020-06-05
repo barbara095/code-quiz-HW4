a8ad06c63b71285d75fe12a5f222d0ac6e272ea4
Barbs <barbarap095@gmail.com>
2020-06-05 17:55:39 +1000

    Commit changes

--- script.js
+++ script.js
@@ -30,22 +30,22 @@ var questionsArray = [
     {
         question: "Which javascript object allows us to ask a user a closed question? i.e: they can only answer 'yes' or 'no', 'true' or 'false.' ",
         choiceA: "strings",
-        choiceB: "booleans", 
-        choiceC: "alerts", 
+        choiceB: "booleans",
+        choiceC: "alerts",
         choiceD: "numbers",
         answer: "booleans",
     },
     {
         question: "What is the main advantage of using jQuery over standard javascript?",
-        choiceA: "gives you wings", 
+        choiceA: "gives you wings",
         choiceB: "simplifies DOM manipulation and event handling, thus reducing the amount of lines needed to code",
-        choiceC: "allows you to debug elements quicker", 
+        choiceC: "allows you to debug elements quicker",
         choiceD: "enables you to set and retrieve items from local storage",
         answer: "simplifies DOM manipulation and event handling, thus reducing the amount of lines needed to code",
     },
     {
         question: "What is the difference between Math.ceil() and Math.floor()?",
-        choiceA: "Math.ceil() rounds a number up to the next largest integer, while Math.floor() returns the largest integer less than or equal to any given number", 
+        choiceA: "Math.ceil() rounds a number up to the next largest integer, while Math.floor() returns the largest integer less than or equal to any given number",
         choiceB: "Math.ceil() console logs a list starting from the highest to the lowest value, while Math.floor() console logs a list from lowest to highest",
         choiceC: "All of these",
         choiceD: "None of these",
@@ -53,32 +53,32 @@ var questionsArray = [
     },
     {
         question: "True or false: A child function can access objects from its grandchild function",
-        choiceA: "True", 
+        choiceA: "True",
         choiceB: "False",
         answer: "False"
     },
     {
         question: "In Javascript, what does a 'do, while' statement execute?",
-        choiceA: "executes a function until that function is true", 
-        choiceB: "creates a loop that executes a statement until the condition outputs as 'false", 
-        choiceC: "nothing, just sits there in the background as a distraction", 
+        choiceA: "executes a function until that function is true",
+        choiceB: "creates a loop that executes a statement until the condition outputs as 'false",
+        choiceC: "nothing, just sits there in the background as a distraction",
         choiceD: "goes through an array until the last element of the array is shown",
         answer: "creates a loop that executes a statement until the condition outputs as 'false'",
     },
     {
         question: "How many columns does Bootstrap's grid system typically have?",
-        choiceA: "6", 
-        choiceB: "8", 
-        choiceC: "10", 
+        choiceA: "6",
+        choiceB: "8",
+        choiceC: "10",
         choiceD: "12",
         answer: "12",
     },
 
     {
         question: "Finish this sentence: children elements are only HTML elements, while a child element is______",
-        choiceA: "nothing", 
-        choiceB: "only limited to its parent", 
-        choiceC: "nothing, just sits there in the background as a distraction", 
+        choiceA: "nothing",
+        choiceB: "only limited to its parent",
+        choiceC: "nothing, just sits there in the background as a distraction",
         choiceD: "everything",
         answer: "everything",
     }
@@ -102,10 +102,11 @@ function startQuiz() {
     timerEl = setInterval(clockTick, 1000);
 
     // show starting time
-    timerEl.textContent = timeElapsed;
+    timerEl.textContent = totalSeconds;
 
     getQuestion();
-    // setTimer();
+
+    renderCounter();
 }
 
 function getQuestion() {
@@ -119,77 +120,54 @@ function getQuestion() {
     choiceC.innerHTML = questionDisplay.choiceC;
     choiceD.innerHTML = questionDisplay.choiceD;
 
-    // Create a loop that goes through each question
 }
 
-function validateAnswer(choice) {
-    if (choice === questionsArray[currentQuestion].answer) {
-        // If choice selected is correct, increment score by 6 points
-        score += 6;
-    } else {
-        // Otherwise decrement time by 8 seconds
-        secondsElapsed -= 8;
-    }
-        currentQuestion++;
-        renderQuestion();
-        // else ends the quiz and shows the resultsDiv
-        stopTimer();
-       
-}
-
-// Sets the totalSeconds
-function setTimer() {
-    // Clears the quizTimer
-    clearInterval(quizTimer);
-    totalSeconds = 60;
+function renderCounter() {
+    // Clears the quizTimer & sets the totalSeconds
+    setTime();
+  
+    // Increases secondsElapsed by 1 second which gets subtracted from the totalSeconds set in setTime()
+    quizTimer = setInterval(function () {
+      secondsElapsed++;
+  
+      renderTime();
+    }, 1000);
   }
 
-function setTimer() {
-    setTime();
+function setTime() {
+    var timerInterval = setInterval(function () {
+        secondsElapsed++;
+        totalSeconds = 60;
+        timerEl.textContent = totalSeconds;
 
-    // We only want to start the timer if the time = 0
-    if (timerEl === 0) {
-        
-        interval = setInterval(function () {
-            timeElapsed++;
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+        }
 
-            // Call renderTime for every second elapsed.
-            renderTime();
-        }, 1000);
-    }
+    }, 1000);
 }
 
 
 function questionClick() {
     // check if user guessed wrong
-    if (this.value !== questions[currentQuestion].answer) {
-        // penalize time
-        time -= 15;
-
-        if (time < 0) {
-            time = 0;
-        }
+    if (this.value == questionArray[currentQuestion].answer) {
+        alert("Correct!")
+        // If choice selected is correct, increment score by 6 points
+        score += 6;
+    } else {
+        alert("Sorry, that is incorrect I'm afraid...")
+        // Otherwise decrement time by 8 seconds
+        secondsElapsed -= 8;
 
         // display new time on page
         timerEl.textContent = time;
 
-        feedbackEl.textContent = "Wrong!";
-    } else {
-    
-        feedbackEl.textContent = "Correct!";
     }
-
-    // flash right/wrong feedback on page for half a second
-    feedbackEl.setAttribute("class", "feedback");
-    setTimeout(function () {
-        feedbackEl.setAttribute("class", "feedback hide");
-    }, 1000);
-
     // move to next question
     currentQuestion++;
 
     // check if we've run out of questions
-    if (currentQuestionIndex === questions.length) {
+    if (currentQuestion === questions.length) {
         quizEnd();
     } else {
         getQuestion();
