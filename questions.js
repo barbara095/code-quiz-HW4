702595d640d0292fc35b1c36e119c0b4a4fc2d0e
Samuel Pai <samuelpai16@gmail.com>
2020-10-26 17:17:20 -0700

    added a new file called quesitons.js that contains more questions about JS that can be swapped out when needed

---
+++ questions.js
@@ -0,0 +1,61 @@
+var questions = [
+    {
+        number: 1,
+        question: "Which HTML element do we put the JavaScript src in?",
+        choiceA: "<body>",
+        choiceB: "<head>",
+        choiceC: "<html>",
+        choiceD: "<script>",
+        answer: "D",
+    },
+    {
+        number: 2,
+        question: "How do you define a function declaration named helloworld in JavaScript?",
+        choiceA: "function helloworld(){}",
+        choiceB: "helloworld function(){}",
+        choiceC: "helloworld(){}",
+        choiceD: "var helloworld = function(){}",
+        answer: "A",
+    },
+    {
+        number: 3,
+        question: "How do you pass a JavaScript function called test by reference?",
+        choiceA: "You need to execute the function first test()",
+        choiceB: "You cannot",
+        choiceC: "You simply write the name of the function without the parentheses at the end like test",
+        choiceD: "None of these",
+        answer: "C",
+    },
+    {
+        number: 4,
+        question: "Function expressions are hoisted in JavaScript",
+        choiceA: "True",
+        choiceB: "False",
+        answer: "B"
+    },
+    {
+        number: 4,
+        question: "In a JavaScript object, if the key and value pair are identical, you can just write the value one time without the colon",
+        choiceA: "True",
+        choiceB: "False",
+        answer: "A"
+    },
+    {
+        number: 6,
+        question: "How do you add an in line comment in JS? ",
+        choiceA: "//",
+        choiceB: "///",
+        choiceC: "$",
+        choiceD: "#",
+        answer: "A",
+    },
+    {
+        number: 7,
+        question: "How can you run JavaScript outside of the browser?",
+        choiceA: "You can't",
+        choiceB: "By using a runtime environment like NodeJS",
+        choiceC: "Using ReactJS",
+        choiceD: "By using Internet Explorer",
+        answer: "B",
+    }
+]
